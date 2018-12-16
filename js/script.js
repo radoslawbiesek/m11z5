ce80e36983abbc4f4bb35e14c76ed6a39e33b0f0
Radosław Biesek <radoslaw.biesek@gmail.com>
2018-12-16 20:22:41 +0100

    Create 2 instances of class Button

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 'use strict';
 (function() {
-function Button(text) {
-	this.text = text || 'Hello';
-};
-Button.prototype = {
-    create: function() {
-        var self = this;
-        this.element = document.createElement('button');
-        this.element.innerText = this.text;
-        this.element.addEventListener('click', function() {
-            alert(self.text);
-        });
-        document.body.appendChild(this.element);
-    }
-};
+    function Button(text) {
+        this.text = text || 'Hello';
+    };
+    Button.prototype = {
+        create: function() {
+            var self = this;
+            this.element = document.createElement('button');
+            this.element.innerText = this.text;
+            this.element.addEventListener('click', function() {
+                alert(self.text);
+            });
+            document.body.appendChild(this.element);
+        }
+    };
+
+    var btn1 = new Button('Button 1');
+    btn1.create();
+    var btn2 = new Button('Button 2');
+    btn2.create();
 
 })();
